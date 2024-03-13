f0ff60275ad7f63a2ce2033c16fb79e1b1afe0aa
HKK <heinkaungkhant1999@gmail.com>
2024-03-13 16:54:10 +0700

    Display platfrom icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: {platform: Platform}[]
 }
 
 interface FetchGamesResponse {
@@ -22,7 +30,10 @@ function useGames() {
     apiClient
       .get<FetchGamesResponse>('/games', {signal: controller.signal})
       .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message)
+      });
     
     return () => controller.abort();
   }, []);
