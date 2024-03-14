4a7d8ebeb01aaeb61373a548965816acb8f4e6eb
HKK <heinkaungkhant1999@gmail.com>
2024-03-14 15:46:47 +0700

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,8 @@
+import noImage from '../assets/no-image-placeholder.webp';
 
 function getCroppedImageUrl(url: string) {
-  if (!url) return '';
-  
+  if (!url) return noImage;
+
   const target = 'media/';
   const index = url.indexOf(target) + target.length;
   
